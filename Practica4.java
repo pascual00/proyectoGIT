0dd1f3a0f6056bb58dc8af64e828ffb1aabbfb3c
Pascual Vicedo <pascualvicedo.alu@iespacomolla.es>
2019-10-18 20:00:11 +0200

    Hemos añadidio que nos muestre el resto de una division y hemos añadido el programa de las notas

---
+++ Practica4.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Practica4 {
+
+	public static void main(String [] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Introduce tu nota: ");
+		double nota = input.nextDouble();
+
+		if (nota > 10) {
+
+			System.out.print("Incorrecto\n");
+
+		} else if (nota >= 9 && nota <= 10) {
+
+			System.out.print("Excelente\n");
+
+		} else if (nota >= 6.5 && nota < 9) {
+
+			System.out.print("Notable\n");
+
+		} else if (nota >= 5 && nota < 6.5) {
+
+			System.out.print("Aprobado\n");
+
+		} else if (nota >= 0 && nota < 5) {
+
+			System.out.print("Suspendido\n");
+
+		} else {
+
+			System.out.print("Incorrecto\n");
+
+		}
+	}
+}
